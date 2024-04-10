9089fc11b15bcd3c10aa3f4629fd67ff88e77601
Mikhail Lyashkov <iwishcoolwork@gmail.com>
2024-04-10 16:28:31 +0700

    prepared data pipeline

--- data_processing.py
+++ data_processing.py
@@ -38,14 +38,3 @@ def decode_and_resize(img_path):
     return img
 
 
-def process_input(img_path, captions):
-    return decode_and_resize(img_path), vectorization(captions)
-
-
-def make_dataset(images, captions):
-    dataset = tf.data.Dataset.from_tensor_slices((images, captions))
-    dataset = dataset.shuffle(BATCH_SIZE * 8)
-    dataset = dataset.map(process_input, num_parallel_calls=AUTOTUNE)
-    dataset = dataset.batch(BATCH_SIZE).prefetch(AUTOTUNE)
-
-    return dataset
