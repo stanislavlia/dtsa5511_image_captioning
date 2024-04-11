4626d175e77a80772723fc70bd4235192baff1fe
Mikhail Lyashkov <iwishcoolwork@gmail.com>
2024-04-11 13:13:15 +0700

    in example, y is tensor (batch, 5_comments, length); in my case, example is 1 image - 1 caption; need to adjust code

--- train_config.py
+++ train_config.py
@@ -1,18 +1,35 @@
 import tensorflow as tf
+import datetime
 
 ###CONSTANTS
 DATA_PATH = "data/flickr30k_images/"
 IMAGES_PATH = "data/flickr30k_images/flickr30k_images/"
 IMAGE_SIZE=(224, 224)
 VAL_FRACTION=0.05
-SEQ_LENGTH=80
+SEQ_LENGTH=32
 VOCAB_SIZE=10000
 BATCH_SIZE=64
 EPOCHS=20
 AUTOTUNE=tf.data.AUTOTUNE
 
-#preprocessing
+EMBED_DIM=512
+FF_DIM=256
 
 STRIP_CHARS = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 ###
+
+def get_config():
+    return {
+        "TIMESTAMP" : datetime.datetime.now(),
+        "DATA_PATH": DATA_PATH,
+        "IMAGES_PATH": IMAGES_PATH,
+        "IMAGE_SIZE": IMAGE_SIZE,
+        "VAL_FRACTION": VAL_FRACTION,
+        "SEQ_LENGTH": SEQ_LENGTH,
+        "VOCAB_SIZE": VOCAB_SIZE,
+        "BATCH_SIZE": BATCH_SIZE,
+        "EPOCHS": EPOCHS,
+        "AUTOTUNE": AUTOTUNE,
+        "STRIP_CHARS": STRIP_CHARS
+    }
