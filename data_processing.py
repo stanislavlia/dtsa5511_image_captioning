89da9acb4d6238c4b087d284e9a4f896f045c00f
Mikhail Lyashkov <iwishcoolwork@gmail.com>
2024-04-11 20:03:51 +0700

    added function to generate; need change it a bit

--- data_processing.py
+++ data_processing.py
@@ -34,7 +34,7 @@ def decode_and_resize(img_path):
     img = tf.io.read_file(img_path)
     img = tf.image.decode_jpeg(img, channels=3)
     img = tf.image.resize(img, IMAGE_SIZE)
-    img = img / 255.0 
+    #img = img / 255.0 
     img = tf.image.convert_image_dtype(img, tf.float32)
     return img
 
